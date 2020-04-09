dc430aaf150cb61777699fe4c6744027c88d9d78
Lucjan Dudek <lucekdudek@gmail.com>
2020-04-09 12:27:50 +0200

    Added sign out button and view

--- precioussecret/client/urls.py
+++ precioussecret/client/urls.py
@@ -5,6 +5,7 @@ from precioussecret.client.views import AddSecretView
 from precioussecret.client.views import AccessSecretView
 from precioussecret.client.views import HomeView
 from precioussecret.client.views import LoginView
+from precioussecret.client.views import LogoutView
 from precioussecret.client.views import SecretDetailsView
 
 app_name = 'client'
@@ -12,6 +13,7 @@ app_name = 'client'
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
     url(r'^add-secret/$', login_required(AddSecretView.as_view()), name='add-secret'),
     url(r'^secret-details/$', SecretDetailsView.as_view(), name='secret-details'),
     url(r'^access-secret/(?P<access_name>[\w-]+)/$', AccessSecretView.as_view(), name='access-secret'),
